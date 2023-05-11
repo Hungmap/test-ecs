74940b64e199b07dc0d8e81f38287f19f4ae265f
Hungmap <67423606+Hungmap@users.noreply.github.com>
2023-05-11 09:58:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# testcicd
+# ecs-demo
---
+++ index.js
@@ -0,0 +1,23 @@
+import  Express  from "express";
+import os from "os";
+const app = Express ();
+app.get('/', (req, res) => {
+    const containerip = req.socket.localAddress;
+    const clienip = req.header('x-forwarded-for');
+    const elbip = req.socket.remoteAddress;
+    const hostname = os.hostname();
+    console.log('Hello NodeJS from Project-network')
+    res.json({
+        serviceName : 'esc-demo',
+        contact: 'nguyenthanhhung021199@gmail.com',
+        Cientip : clienip,
+        ALB : elbip,
+        Containerip : containerip,
+        containerName: hostname,
+
+
+    });
+});
+app.listen(8080,()=>{
+    console.log("app stated successfully");
+});
