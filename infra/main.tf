f6ae852438b1ee07cf6f1246e1d67c8a0207cc0d
Hungmap <67423606+Hungmap@users.noreply.github.com>
2024-02-27 11:09:15 +0700

    update terrafomt

--- infra/main.tf
+++ infra/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.55.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-southeat-1"
+  region = "ap-southeast-1"
 }
 module "vpc" {
   source         = "./vpc"
